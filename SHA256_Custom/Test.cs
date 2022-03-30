a9f364f4eb9ef443c89aeb2e171ac746f4ec221f
mason1225 <75379891+mason1225@users.noreply.github.com>
2022-03-30 06:06:26 -0400

    Cleaned up old code

--- SHA256_Custom/Test.cs
+++ SHA256_Custom/Test.cs
@@ -5,31 +5,6 @@ namespace SHA256_Custom
 {
     class Test
     {
-        /*
-        // TEST SINGLE PHRASE
-        public static bool EvaluateHash(string phrase)
-        {
-            SHA256 hash = SHA256.Create();
-            byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(phrase));
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                builder.Append(bytes[i].ToString("x2"));
-            }
-
-            string libraryHash = builder.ToString();
-            string customHash = Sha256Custom.processChunks(phrase);
-
-            if (!libraryHash.Equals(customHash))
-            {
-                return false;
-            }
-
-            return true;
-        }
-        */
-
-
         // TEST SINGLE PHRASE MULTIPLE TIMES
         public static bool EvaluateHash(string phrase, int iterations = 1)
         {
